d4751618b467f6c7b828ca69661fe402c9f02b00
vamon1122 <35330025+vamon1122@users.noreply.github.com>
2018-06-08 21:01:18 +0100

    Add project files.

---
+++ BenCsv/CsvReader.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenLog;
+
+namespace BenCsv
+{
+    public static class CsvReader
+    {
+        static LogWriter MyLog;
+
+        static CsvReader()
+        {
+            MyLog = new LogWriter("BenCsv.log", AppDomain.CurrentDomain.BaseDirectory);
+        }
+
+        public static bool CanReadFile(string pDir)
+        {
+            try
+            {
+                ParseFile(new StreamReader(pDir));
+            }
+            catch
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static List<string[]> ReadFile(string pDir)
+        {   
+            if(pDir.First() == '"')
+            {
+                pDir = pDir.Substring(1, pDir.Length - 1);
+            }
+
+            if (pDir.Last() == '"')
+            {
+                pDir = pDir.Substring(0, pDir.Length - 1);
+            }
+
+            return ParseFile(new StreamReader(pDir));
+        }
+
+        private static List<string[]> ParseFile(StreamReader pReader)
+        {
+            MyLog.Info("Parsing CSV file...");
+            List<string[]> MyLines = new List<string[]>();
+
+            using (pReader)
+            {
+                while (!pReader.EndOfStream)
+                {
+                    MyLines.Add(ParseCsvLine(pReader.ReadLine()));
+                    MyLog.Break();
+                }
+            }
+            MyLog.Info("Successfully parsed CSV line!");
+            return MyLines;
+        }
+
+        private static string[] ParseCsvLine(string pLine)
+        {
+            //MyLog.Info("Parsing CSV line...");
+            //MyLog.Debug(String.Format("Parsing line \"{0}\"", pLine));
+
+            var Line = pLine;
+            string[] Values;
+
+
+            List<string> TempValues = new List<string>();
+            //1.8.10 - Added this if statement. Sorts out values which have commas in them!
+            int Zero = 0;
+            int ValueNo = 0;
+                while (Line.Length != 0)
+                {
+
+
+                    //MyLog.Debug(String.Format("LINE VALUE {0}", ValueNo));
+                    ValueNo++;
+
+                    //Get rid of the comma if it's the next value on the line
+                    if (Line.Contains(","))
+                    {
+                        if (Line.IndexOf(",") == 0)
+                        {
+
+                            Line = Line.Substring(1, Line.Length - 1);
+
+                        }
+                    }
+
+                    //Gets the next value
+                    string ActiveVal;
+                    if (Line.Contains(","))
+                    {
+                        //MyLog.Debug("There are more commas to parse.");
+                        ActiveVal = Line.Substring(Zero, Line.IndexOf(","));
+                    }
+                    else
+                    {
+                        //MyLog.Debug("There are NO more commas to parse.");
+                        ActiveVal = Line;
+                    }
+
+
+                    //MyLog.Debug(String.Format("NEXT ActiveVal = \"{0}\"", ActiveVal));
+                    Line = Line.Substring(ActiveVal.Length, Line.Length - ActiveVal.Length);
+                    //MyLog.Debug(String.Format("Meaning that Line now = \"{0}\"", Line));
+
+                    if (ActiveVal.Contains('"'))
+                    {
+                        if (ActiveVal.Length >= 2)
+                        {
+                            if (ActiveVal.Substring(ActiveVal.Length - 1, 1) == "\"" && (ActiveVal.Split('"').Length - 1) % 2 == 0) //Added the second argument because if there is an odd number
+                            {                                                                                                       //the string is not complete (some values were being marked
+                                //MyLog.Debug(String.Format("ActiveVal \"{0}\" is already complete!", ActiveVal));                    //as complete when they weren't
+
+                            }
+                            else
+                            {
+                                CompleteLine();
+                            }
+                        }
+                        else
+                        {
+                            CompleteLine();
+                        }
+                    }
+                    else
+                    {
+                        MyLog.Debug(String.Format("ActiveVal \"{0}\" is already complete!", ActiveVal));
+                    }
+
+                    void CompleteLine()
+                    {
+                        MyLog.Debug(String.Format("ActiveVal \"{0}\" is NOT complete. Completing ActiveVal...", ActiveVal));
+                        if (Line.Contains("\","))
+                        {
+                            //MyLog.Debug("Line contains \",");
+                            //MyLog.Debug("Index of \", = " + Line.IndexOf("\","));
+
+                            ActiveVal += Line.Substring(0, Line.IndexOf("\",") + 1);
+                            Line = Line.Substring(Line.IndexOf("\",") + 1, Line.Length - Line.IndexOf("\",") - 1); //Added the +1/-1 because " wasn't being removed
+
+                        }
+                        else
+                        {
+                            //MyLog.Debug("Line does not contain \",");
+                            ActiveVal += Line;
+                            Line = "";
+                        }
+
+
+                        if ((ActiveVal.Split('"').Length - 1) % 2 != 0)
+                        {
+                            //MyLog.Debug(String.Format("ActiveVal still is incomplete. ActiveVal = \"{0}\"", ActiveVal));
+                            //MyLog.Debug(String.Format("Line = \"{0}\"", Line));
+                            CompleteLine();
+                        }
+                        else
+                        {
+                            //MyLog.Debug(String.Format("Completed ActiveVal successfully! ActiveVal = \"{0}\"", ActiveVal));
+                            //MyLog.Debug(String.Format("Line = \"{0}\"", Line));
+                        }
+                    }
+
+                    if (ActiveVal.Length > 0) //Fixes bug where file would not be imported if value was blank
+                    {
+                        if (ActiveVal[0] == '"')
+                        {
+                            //MyLog.Debug("ActiveVal starts with a quotation mark. Removing quotation mark...");
+                            ActiveVal = ActiveVal.Substring(1, ActiveVal.Length - 1);
+                            //MyLog.Debug(String.Format("Successfully removed quotation mark. ActiveVal now = \"{0}\"", ActiveVal));
+
+                        }
+
+                        if (ActiveVal.Last() == '"')
+                        {
+                            //MyLog.Debug("ActiveVal ends with a quotation mark. Removing quotation mark.");
+                            ActiveVal = ActiveVal.Substring(0, ActiveVal.Length - 1);
+                            //MyLog.Debug(String.Format("Successfully removed quotation mark. ActiveVal now = \"{0}\"", ActiveVal));
+                        }
+
+                        //Quotation marks (") are saved as ("") in CSV files. This changes them back to (")
+                        if (ActiveVal.Contains('"'))
+                        {
+                            //MyLog.Debug("Formatting quotation marks...");
+                            ActiveVal = ActiveVal.Replace("\"\"", "\"");
+                            //MyLog.Debug(String.Format("Formatted quotation marks successfully! ActiveVal now = \"{0}\"", ActiveVal));
+                        }
+                    }
+
+                    //MyLog.Debug("Adding string to TempValues...");
+                    TempValues.Add(ActiveVal);
+                    //MyLog.Debug("Successfully added string to TempValues");
+                }
+                Values = TempValues.ToArray();
+                return Values;
+            }
+
+            
+        }
+    }
---
+++ TestCsv/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenCsv;
+
+namespace TestCsv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("CSV Dir: ");
+            List<string[]> MyCsv = CsvReader.ReadFile(Console.ReadLine());
+
+            string[] Alphabet = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+            int Line = 0;
+
+            foreach(string[] CsvLine in MyCsv)
+            {
+                int Cell = 0;
+                foreach(string CsvCell in CsvLine)
+                {
+                    if(Alphabet[Cell] + (Line + 1) == CsvCell)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                    }
+                    Console.WriteLine(String.Format("{0} = {1}",Alphabet[Cell] + (Line + 1), CsvCell));
+                    Cell++;
+                }
+                Line++;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
